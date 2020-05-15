42660575dd0ec8af55218c9e86124178216863ee
Christina Dominguez <domingcd@gmail.com>
2020-05-15 12:53:40 -0400

    fixing confusion matrix

--- src/transformers/data/metrics/__init__.py
+++ src/transformers/data/metrics/__init__.py
@@ -13,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import sys
+sys.setrecursionlimit(10000)
 
 try:
     from scipy.stats import pearsonr, spearmanr
